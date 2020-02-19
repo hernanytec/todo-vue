33b066c8ef74a5aeb0e821816d5a8739886287fa
Hernany <hernanytec777@gmail.com>
2020-02-19 14:11:48 -0300

    fix count complete bug on add a new todo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Vue.component('todo-item', {
     template: `
         <li>
             {{ todo.text }}
-            <input type="checkbox" v-model="todo.completed" v-on:change="updateComplete" title="Marcar como concluído"/>
+            <input type="checkbox" v-model="todo.completed" v-on:change="markItem(index)" title="Marcar como concluído"/>
             <button class="delete" v-on:click="deleteTodo(index)">deletar</button>
         </li>
     `,
@@ -11,14 +11,11 @@ Vue.component('todo-item', {
     methods:{
         deleteTodo: function(idx){
             this.$root.todos.splice(idx, 1)
-            this.updateComplete()
+            this.$root.updateComplete()
         },
-        updateComplete: function(){
-            const size = this.$root.todos.length
-            const marked = this.$root.todos.filter((todo)=> todo.completed).length
-            
-            const total =  size === 0 ? 0 : parseFloat(marked / size * 100).toFixed(1)  
-            this.$root.totalComplete = total
+        markItem: function(idx){
+            this.$root.todos[idx].marked = !this.$root.todos[idx].marked 
+            this.$root.updateComplete()
         }
     }
 })
@@ -45,7 +42,15 @@ var app = new Vue({
             else{
                 this.todos.push({text: this.newTodo, completed: false})
                 this.newTodo = ""
+                this.updateComplete()
             }
         },
+        updateComplete: function(){
+            const size = this.todos.length
+            const marked = this.todos.filter((todo)=> todo.completed).length
+            
+            const total =  size === 0 ? 0 : parseFloat(marked / size * 100).toFixed(1)  
+            this.$root.totalComplete = total
+        }
     }
 });
